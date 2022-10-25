8f18030f817d7af55343f7a1c8d0d976f79c0910
sjmin <msj3001@naver.com>
2022-10-25 16:55:34 +0900

    function

---
+++ Part.01/return_with_data.py
@@ -0,0 +1,5 @@
+def reaturn_test():
+    return 100
+
+value = reaturn_test()
+print(value)
---
+++ Part.01/variable_param.py
@@ -0,0 +1,9 @@
+def print_n_times(n, *values):
+    #n번 반복
+    for i in range(n):
+        #values는 리스트처럼 활용
+        for value in values:
+            print(value)
+        print()
+
+print_n_times(3, "안녕하세요", "즐거운", "파이썬 프로그래밍")
