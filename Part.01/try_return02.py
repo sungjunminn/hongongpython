25aff87fbcf36d78f3118099a993908f1acec4cd
sjmin <msj3001@naver.com>
2022-10-28 10:26:07 +0900

    except

---
+++ Part.01/try_return02.py
@@ -0,0 +1,12 @@
+def write_text_file(filename, text):
+    try:
+        file = open(filename, "w")
+        return
+
+        file.write(text)
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
+
+write_text_file("test.txt", "안녕하세요!")
