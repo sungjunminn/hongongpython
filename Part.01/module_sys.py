3a2b6063dc688eb3526d265686984e5a1df53277
sjmin <msj3001@naver.com>
2022-10-31 12:54:07 +0900

    module

---
+++ Part.01/module_sys.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.argv)
+
+print("---")
+
+print("gewindowsversion:()", sys.getwindowsversion())
+print("---")
+print("copyright:", sys.copyright)
+print("---")
+print("version:", sys.version)
+
+sys.exit()
